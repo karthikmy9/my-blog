512a7d2b16d8117a9f35a50bc02e0b55de041f65
Karthik Dheeraj <karthikmy9@gmail.com>
2020-01-30 08:35:38 +0530

    First commit

---
+++ src/build/precache-manifest.d1f445952fcb882e3f188e3b5b60da02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "298e155d34f0e0d4bd912f0eb1dfe36a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12a8e9f8f004e20446b5",
+    "url": "/static/css/main.185bb295.chunk.css"
+  },
+  {
+    "revision": "fa364d64b048247fef48",
+    "url": "/static/js/2.9b8cc8f9.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.9b8cc8f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "12a8e9f8f004e20446b5",
+    "url": "/static/js/main.65a6ec1b.chunk.js"
+  },
+  {
+    "revision": "f14d8c5b74cf6ba67756",
+    "url": "/static/js/runtime-main.2a2960a5.js"
+  }
+]);
